c5912fc570d497982b931ede4713ed692f1ae41a
chiro <peanut2626@gmail.com>
2014-09-22 21:17:40 +0900

    Move some functions to the library.

--- src-exec/Main.hs
+++ src-exec/Main.hs
@@ -2,21 +2,15 @@
 
 module Main where
 
-import Control.Monad (join, when)
+import Control.Monad (join)
 
 import qualified Data.ByteString.Char8 as C
 import qualified Data.HashMap.Strict as H
-import Data.Maybe (isJust, fromJust)
 import Data.Text (Text())
 import qualified Data.Text as T
 import qualified Data.Yaml as Y
 
 import Options.Applicative
-
-import System.Process
-import System.Environment
-import System.IO
-import System.IO.Temp
 
 import Pit
 
@@ -36,42 +30,16 @@ opts = subparser (
 printYaml :: Y.Value -> IO ()
 printYaml yaml = putStrLn $ C.unpack $ Y.encode yaml
 
-openEditorAndGetNewValue :: Maybe Y.Value -> IO (Maybe Y.Value)
-openEditorAndGetNewValue def = do
-  editor' <- lookupEnv "EDITOR"
-  tty <- hIsTerminalDevice stdout
-  if isJust editor' && tty
-    then withSystemTempFile "new.yaml" $ \path h -> do
-    hClose h
-    when (isJust def) $ do
-      let content = C.unpack $ Y.encode $ fromJust def
-      writeFile path content
-    _ <- callCommand (fromJust editor' ++ " " ++ path)
-    Y.decodeFile path
-    else return Nothing
-
 getCommand :: Text -> IO ()
 getCommand key = do
-  v <- Pit.get key :: IO (Maybe Y.Value)
-  case v of
-   Nothing -> do
-     v' <- openEditorAndGetNewValue Nothing
-     case v' of
-      Nothing -> putStrLn "Failed to get the value."
-      Just v'' -> do
-        Pit.set key v''
-        printYaml v''
-   Just v' -> printYaml v'
+  v <- Pit.get key Y.Null
+  printYaml v
 
 setCommand :: Text -> IO ()
 setCommand key = do
-  v <- Pit.get key :: IO (Maybe Y.Value)
-  v' <- openEditorAndGetNewValue v
-  case v' of
-   Nothing -> putStrLn "Failed to set the value."
-   Just v'' -> do
-     Pit.set key v''
-     putStrLn "Succeed to set the value."
+  Pit.set key
+  v <- Pit.get key Y.Null
+  printYaml v
 
 main :: IO ()
 main = join $ execParser (info opts idm)
